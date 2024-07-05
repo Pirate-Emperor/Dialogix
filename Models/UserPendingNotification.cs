48661634a70514171fa18669dcc618ee454eccf5
Pirate-Emperor <piratekingrahul@gmail.com>
2024-07-06 00:34:00 +0100

    Add Models\UserPendingNotification.cs

---
+++ Models/UserPendingNotification.cs
@@ -0,0 +1,11 @@
+﻿namespace Dialogix.Models
+{
+    public class UserPendingNotification
+    {
+        public int? LoginUserID { get; set; }
+        public string? Message { get; set; }
+        public bool? IsSent { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public string? FCMToken { get; set; }
+    }
+}
